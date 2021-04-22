1fb519bfac41fb5c2f44a900bbe4a18cb848c13e
deankrotzer <krotzersoftawre@gmail.com>
2021-04-21 17:30:59 -0700

    v1.0.1

---
+++ build/precache-manifest.ff94e9fdadfd498af9784d8cefbfc2a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1dc6c162abae984837940bb71d1de51",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59ee812ae6c0611e11f0",
+    "url": "./static/css/main.b3209779.chunk.css"
+  },
+  {
+    "revision": "ad3e43ed678a1ba97d85",
+    "url": "./static/js/2.61099d35.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.61099d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ee812ae6c0611e11f0",
+    "url": "./static/js/main.f72c0976.chunk.js"
+  },
+  {
+    "revision": "191ed9450e3b1a883222",
+    "url": "./static/js/runtime-main.6c95075a.js"
+  }
+]);
